d609692b37fcb0b01ae4dcc3ac9d49425dc4c7b8
rizwan-0987 <leocoin4737@gmail.com>
2025-09-18 17:22:52 +0500

    Creating frontend of Devtinde and make login functional

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import { Provider } from "react-redux";
+import { appStore } from "./utils/appSrore";
+import Feed from "./components/Feed";
+import Profile from "./components/Profile";
+
+function App() {
+  return (
+    <>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
+              <Route path="/login" element={<Login />} />
+               <Route path="/profile" element={<Profile/>}/>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Login.jsx
@@ -0,0 +1,104 @@
+import axios from "axios";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+
+const Login = () => {
+    const [emailId, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleLogin = async () => {
+        try {
+            const res = await axios.post(BASE_URL + "/login", {
+                emailId,
+                password,
+            }, { withCredentials: true });
+            dispatch(addUser(res.data));
+            navigate("/")
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
+    return (
+        <div className="flex justify-center">
+            <div className="card bg-base-200 w-96 shadow-sm mt-22 ">
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">Login</h2>
+                    <label className="input validator">
+                        <svg
+                            className="h-[1em] opacity-50"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                        >
+                            <g
+                                strokeLinejoin="round"
+                                strokeLinecap="round"
+                                strokeWidth="2.5"
+                                fill="none"
+                                stroke="currentColor"
+                            >
+                                <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+                                <circle cx="12" cy="7" r="4"></circle>
+                            </g>
+                        </svg>
+                        <input
+                            type="email"
+                            value={emailId}
+                            required
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder="Email"
+                            //   pattern="[A-Za-z][A-Za-z0-9\-]*"
+                            minLength="3"
+                            maxLength="30"
+                            title="Only letters, numbers or dash"
+                        />
+                    </label>
+                    <p className="validator-hint self-start hidden">
+                        Enter Valid Email
+                    </p>
+                    <label className="input validator">
+                        <svg
+                            className="h-[1em] opacity-50"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                        >
+                            <g
+                                strokeLinejoin="round"
+                                strokeLinecap="round"
+                                strokeWidth="2.5"
+                                fill="none"
+                                stroke="currentColor"
+                            >
+                                <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+                                <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+                            </g>
+                        </svg>
+                        <input
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                            placeholder="Password"
+                            minLength="8"
+                            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                            title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+                        />
+                    </label>
+                    <p className="validator-hint hidden">
+                        password must 8 Char
+                    </p>
+                    <div className="card-actions">
+                        <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
